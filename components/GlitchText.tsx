1879f292fe968ee6645460760b16904507dcdd04
MakdumIbrohim <makdumibrohim3@gmail.com>
2025-12-31 22:21:16 +0700

    feat(ui): add glitch text to home page

---
+++ components/GlitchText.tsx
@@ -0,0 +1,11 @@
+const GlitchText = () => {
+  return (
+    <div className="glitch-text">
+      <p className="text-base font-bold .text-shadow-glitch">
+        Glitch Text Effect
+      </p>
+    </div>
+  );
+};
+
+export default GlitchText;
