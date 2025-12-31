371366744c3dcf130dc7343f4d84e7b585f142aa
MakdumIbrohim <makdumibrohim3@gmail.com>
2025-12-31 22:48:54 +0700

    feat(ui): add press start 2p font integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +11,11 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const pressStart = localFont({
+  src: './fonts/press_start_2p/PressStart2P-Regular.ttf',
+  variable: '--font-press-start',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${pressStart.variable} antialiased`}
       >
         {children}
       </body>
