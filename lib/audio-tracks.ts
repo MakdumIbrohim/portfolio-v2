44c62c445ea46e76e67aea1f95f287251910da35
MakdumIbrohim <makdumibrohim3@gmail.com>
2026-02-22 13:19:04 +0700

    feat: Add `AudioTrack` interface and initial audio track data, along with a new profile picture asset.

--- lib/audio-tracks.ts
+++ lib/audio-tracks.ts
@@ -9,31 +9,31 @@ export interface AudioTrack {
 export const audioTracks: AudioTrack[] = [
     {
         id: "track-1",
+        title: "Baby ft. Ludacris",
+        artist: "Justin Bieber",
+        coverUrl: "/music/justin-bieber/baby/https___images.genius.com_01dfdad244aaade8895f737ef1b15562.565x565x1.jpg",
+        audioUrl: "/music/justin-bieber/baby/Justin%20Bieber%20-%20Baby%20ft.%20Ludacris.mp3"
+    },
+    {
+        id: "track-2",
         title: "Favorite Girl",
         artist: "Justin Bieber",
         coverUrl: "/music/justin-bieber/favorite-girl/thumbnailify.com_youtube_thumbnail_Full_HD_2026-02-22_04-41-02.jpg",
         audioUrl: "/music/justin-bieber/favorite-girl/Favorite%20Girl.mp3"
     },
     {
-        id: "track-2",
+        id: "track-3",
         title: "It's Only Me",
         artist: "Kaleb J",
         coverUrl: "/music/kaleb-j/its-only-me/thumbnailify.com_youtube_thumbnail_Full_HD_2026-02-22_04-51-41.jpg",
         audioUrl: "/music/kaleb-j/its-only-me/Kaleb J - It's Only Me Official Lyric Video (English Sub CC).mp3"
     },
     {
-        id: "track-3",
+        id: "track-4",
         title: "Love Me",
         artist: "Justin Bieber",
         coverUrl: "/music/justin-bieber/love-me/maxresdefault.jpg",
         audioUrl: "/music/justin-bieber/love-me/Justin%20Bieber%20-%20Love%20Me.mp3"
-    },
-    {
-        id: "track-4",
-        title: "Baby ft. Ludacris",
-        artist: "Justin Bieber",
-        coverUrl: "/music/justin-bieber/baby/https___images.genius.com_01dfdad244aaade8895f737ef1b15562.565x565x1.jpg",
-        audioUrl: "/music/justin-bieber/baby/Justin%20Bieber%20-%20Baby%20ft.%20Ludacris.mp3"
     },
     {
         id: "track-5",
